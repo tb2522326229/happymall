49cdf64340ac1a863bf2fa1df69d53c88f3fe2c2
e <727056228@qq.com>
2018-04-26 08:49:58 +0800

    添加必要的配置并进行后台mapper的测试

---
+++ happymall/src/test/java/com/mall/test/UserTest.java
@@ -0,0 +1,25 @@
+package com.mall.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mall.dao.UserMapper;
+import com.mall.pojo.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class UserTest {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void testList(){
+		UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+		User user = mapper.selectByPrimaryKey(1);
+		System.out.println(user);
+	}
+}
